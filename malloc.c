db2e179ed3b390cc939bf5787554ba27aa2924fe
Rich Felker <dalias@aerifal.cx>
2019-11-28 19:50:39 -0500

    fix spurious missing-nul-check on free for memaligned memory
    normally zero check bytes are never written except at group init, but
    when producing aligned results via a nominal chunk start at an offset
    for alignment, we can't assume there's already a check byte in place.
    so add it here.
    
    the underlying issue is really that the check should not be performed
    at -4 relative to the nominal allocated object address but rather -4
    relative to the entire slot. as-is, we're only catching overflows that
    extend all the way to the used portion of the next slot. fixing this
    will take more work, but it's not critical to correct operation, only
    an improvement to catch more errors in the caller.

--- malloc.c
+++ malloc.c
@@ -615,10 +615,13 @@ void *memalign(size_t align, size_t len)
 	int idx = get_slot_index(p);
 	size_t stride = get_stride(g);
 	unsigned char *end = g->mem->storage + stride*(idx+1) - 4;
+	size_t adj = -(uintptr_t)p & (align-1);
 
-	p += -(uintptr_t)p & (align-1);
+	if (!adj) return p;
+	p += adj;
 	*(uint16_t *)(p-2) = (p-g->mem->storage)/16U;
 	p[-3] = idx;
+	p[-4] = 0;
 	set_size(p, end, len);
 	return p;
 }
