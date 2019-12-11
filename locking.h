47938143eb1d8c01928f00eec80b4a0796374288
Rich Felker <dalias@aerifal.cx>
2019-12-11 13:26:10 -0500

    add pthread mutex locking backend as alt to rwlock, use it by default
    framework for selecting the backend is not complete and needs thought,
    but this is sufficient for doing some manual testing.

--- locking.h
+++ locking.h
@@ -1,6 +1,42 @@
 #ifndef MALLOC_LOCKING_H
 #define MALLOC_LOCKING_H
 
+#define LOCK_WITH_PTHREAD_MUTEX 1
+
+#if LOCK_WITH_PTHREAD_MUTEX
+
+#include <pthread.h>
+
+#define MT 1
+#define RDLOCK_IS_EXCLUSIVE 1
+
+__attribute__((__visibility__("hidden")))
+extern pthread_mutex_t malloc_lock;
+
+#define LOCK_OBJ_DEF \
+pthread_mutex_t malloc_lock = PTHREAD_MUTEX_INITIALIZER
+
+static inline void rdlock()
+{
+	if (MT) pthread_mutex_lock(&malloc_lock);
+}
+
+static inline void wrlock()
+{
+	if (MT) pthread_mutex_lock(&malloc_lock);
+}
+
+static inline void unlock()
+{
+	if (MT) pthread_mutex_unlock(&malloc_lock);
+}
+
+static inline void upgradelock()
+{
+}
+
+#else
+
 #include <pthread.h>
 
 #define MT 1
@@ -32,5 +68,7 @@ static inline void upgradelock()
 	unlock();
 	wrlock();
 }
+
+#endif
 
 #endif
